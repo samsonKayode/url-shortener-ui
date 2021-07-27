18a44a86fa50a05f8e109f40c6f359db4687b251
kayode <samsonkayode@gmail.com>
2021-07-27 22:57:34 +0100

    initial setup

---
+++ src/main/java/com/url/shortener/ui/UrlShortenerUiApplication.java
@@ -0,0 +1,13 @@
+package com.url.shortener.ui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlShortenerUiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrlShortenerUiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/url/shortener/ui/UrlShortenerUiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.url.shortener.ui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlShortenerUiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
