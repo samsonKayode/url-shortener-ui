7478748bccffdc1aa34eb9b56962a929951b69d0
kayode <samsonkayode@gmail.com>
2021-07-28 04:56:02 +0100

    implemented the encode url UI, built fragments for UI, ehanced controller

--- src/main/java/com/url/shortener/ui/controller/UrlController.java
+++ src/main/java/com/url/shortener/ui/controller/UrlController.java
@@ -1,9 +1,59 @@
 package com.url.shortener.ui.controller;
 
+import com.url.shortener.ui.model.UrlDto;
+import com.url.shortener.ui.service.UrlService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.validation.Valid;
 
 @Controller
-@RequestMapping("")
+@RequestMapping("/shortlink")
 public class UrlController {
+
+    Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Autowired
+    UrlService service;
+
+    String encodedURL ="";
+
+    @GetMapping("/home")
+    public String showHome(Model model) {
+        model.addAttribute("encodedURL", encodedURL);
+        return "home/homepage";
+    }
+
+    @ModelAttribute("urlDto")
+    public UrlDto urlDto(Model model) {
+        model.addAttribute("encodedURL", encodedURL);
+        return new UrlDto();
+    }
+
+
+    @PostMapping("/encode")
+    public String encodeUrl(@Valid @ModelAttribute("urlDto") UrlDto urlDto, BindingResult theBind, RedirectAttributes redirectAttributes, Model model) {
+
+        if (theBind.hasErrors()) {
+
+            redirectAttributes.addFlashAttribute("org.springframework.validation.BindingResult.urlDto", theBind);
+            redirectAttributes.addFlashAttribute("urlDto", urlDto);
+
+            return "redirect:/shortlink/home";
+        }else{
+            encodedURL = service.convertUrl(urlDto);
+            model.addAttribute("encodedURL", encodedURL);
+            //redirectAttributes.addFlashAttribute("encodedURL", encodedURL);
+            return "/home/homepage";
+        }
+    }
 }
--- src/main/java/com/url/shortener/ui/model/UrlDto.java
+++ src/main/java/com/url/shortener/ui/model/UrlDto.java
@@ -1,16 +1,22 @@
 package com.url.shortener.ui.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.URL;
 
 import javax.validation.constraints.Size;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class UrlDto {
 
     @Size(min = 5, message = "url size must be greater than 5")
+    @URL(message = "You have entered an invalid URL")
     private String longUrl;
 }
